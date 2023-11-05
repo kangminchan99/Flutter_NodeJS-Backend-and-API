bf7ff11ffe9dd5000f88334e58db636472355459
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-11-06 00:47:51 +0900

    getProduct

--- Frontend/lib/services/api.dart
+++ Frontend/lib/services/api.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_node_front/model/product_model.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -26,6 +27,39 @@ class Api {
       }
     } catch (e) {
       debugPrint(e.toString());
+    }
+  }
+
+  // get method
+  static getProduct() async {
+    List<Product> products = [];
+
+    var url = Uri.parse("${baseUrl}get_product");
+    try {
+      final res = await http.get(url);
+
+      if (res.statusCode == 200) {
+        var data = jsonDecode(res.body);
+        // for Each - 컬렉션의 각 항목에 대한 반복 작업을 간편하게 처리하는 데 사용됩니다.
+        // 람다함수식으로 표현 (익명 함수)
+        data['products'].forEach(
+          (value) => {
+            products.add(
+              Product(
+                name: value['pname'],
+                desc: value['pdesc'],
+                price: value['pprice'],
+              ),
+            ),
+          },
+        );
+        return products;
+        //
+      } else {
+        //
+      }
+    } catch (e) {
+      print(e.toString());
     }
   }
 }
