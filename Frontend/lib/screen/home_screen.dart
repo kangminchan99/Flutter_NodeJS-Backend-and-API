19fd69386c944373955ff9ed78aceac2cc51fe74
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-11-05 23:10:25 +0900

    on tap) create screen

--- Frontend/lib/screen/home_screen.dart
+++ Frontend/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_node_front/screen/create_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,25 +13,35 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("CREATE"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("READ"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("UPDATE"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("DELETE"),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CreateScreen(),
+                  ),
+                );
+              },
+              child: const Text("CREATE"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("READ"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("UPDATE"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("DELETE"),
+            ),
+          ],
+        ),
       ),
     );
   }
